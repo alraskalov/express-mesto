0b95dd6b751fe9e036fcbe1b917b988cbfa0aed4
Alexander Dolzhenko <raskalovalexander@gmail.com>
2022-02-09 09:05:10 +0300

    refactor: errorCode

--- utils/erros.js
+++ utils/erros.js
@@ -1,15 +1,14 @@
-const ValidationErrorCode = 400;
-const castErrorCode = 400;
+const badRequestErrorCode = 400;
 const serverErrorCode = 500;
 
 const checkError = (err, res) => {
   if (err.name === 'ValidationError') {
     res
-      .status(ValidationErrorCode)
+      .status(badRequestErrorCode)
       .send({ message: 'Отправленные данные неверны' });
   } else if (err.name === 'CastError') {
     res
-      .status(castErrorCode)
+      .status(badRequestErrorCode)
       .send({ message: 'Запрашиваемый ресурс не существует' });
   } else {
     res.status(serverErrorCode).send({ message: 'Ошибка сервера' });
