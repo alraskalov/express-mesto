82619eb3dff5381dddba6fe30ce98c345d1b7afc
Alexander Dolzhenko <raskalovalexander@gmail.com>
2022-03-07 18:31:40 +0300

    refactor

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,5 @@
 /* eslint-disable consistent-return */
+const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/AuthError');
 
@@ -14,7 +15,7 @@ module.exports.auth = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'super-strong-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret');
   } catch (err) {
     throw new AuthError('Необходима авторизация');
   }
