eb3a0c2244bad27fc494045921c0ac4fab2c4be5
Alexander <raskalovalexander@gmail.com>
2022-05-02 20:20:06 +0300

    refactor: clear code

--- app.js
+++ app.js
@@ -12,20 +12,12 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000, DB_URL } = process.env;
 
 const app = express();
+mongoose.connect(DB_URL || 'mongodb://localhost:27017/mestodb');
+app.use(requestLogger);
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-mongoose.connect(DB_URL || 'mongodb://localhost:27017/mestodb');
-
-app.use(requestLogger);
-
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
 
 app.post(
   '/signup',
