5a56329c574f4b8dd13ed1ace0f3beddcedc797d
Alexander <raskalovalexander@gmail.com>
2022-05-02 19:58:48 +0300

    fix

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const { auth } = require('./middlewares/auth');
 const NotFoundError = require('./errors/NotFoundError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_URL } = process.env;
 
 const app = express();
 
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect(DB_URL || 'mongodb://localhost:27017/mestodb');
 
 app.use(requestLogger);
 
